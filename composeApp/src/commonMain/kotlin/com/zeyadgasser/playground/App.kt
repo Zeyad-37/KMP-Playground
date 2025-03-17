0e05911e7dc96d1ccc500c7c3773c62cf1892b9e
zeyadg37 <zeyad.gasser@gmail.com>
2025-03-17 19:03:57 +0100

    feat: Remove platform-specific code and update UI
    - Removes `Platform`, `Greeting` classes and its implementation.
    - Removes platform-specific code for Android, iOS, and Desktop.
    - Updates `App` composable to use `Navigator` for navigation.
    - The `Navigator` uses `ListScreen` as the default destination.

--- composeApp/src/commonMain/kotlin/com/zeyadgasser/playground/App.kt
+++ composeApp/src/commonMain/kotlin/com/zeyadgasser/playground/App.kt
@@ -1,41 +1,13 @@
 package com.zeyadgasser.playground
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
 import com.zeyadgasser.playground.sharedUI.theme.AppTheme
 import com.zeyadgasser.playground.tasks.presentation.list.ui.ListScreen
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import kmpplayground.composeapp.generated.resources.Res
-import kmpplayground.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     AppTheme { Navigator(ListScreen) }
-//    MaterialTheme {
-//        var showContent by remember { mutableStateOf(false) }
-//        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-//            Button(onClick = { showContent = !showContent }) {
-//                Text("Click me!")
-//            }
-//            AnimatedVisibility(showContent) {
-//                val greeting = remember { Greeting().greet() }
-//                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-//                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-//                    Text("Compose: $greeting")
-//                }
-//            }
-//        }
-//    }
 }
--- composeApp/src/commonMain/kotlin/com/zeyadgasser/playground/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.zeyadgasser.playground
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
