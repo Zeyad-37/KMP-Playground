28c94efa6331f490c93ce90cb7e56bd663db04db
zeyadg37 <zeyad.gasser@gmail.com>
2025-03-26 11:47:42 +0100

    feat: Add Settings feature module
    -   Introduces a new `:settings` module for managing settings-related functionality.
    -   Adds necessary build configurations and dependencies to the `settings` module:
        -   Utilizes `playground.multiplatform.lib`, `playground.compose.multiplatform`, `playground.koin`, `playground.navigation`, `playground.networking`, `playground.testing` and `kotlinxSerialization` plugins.
        -   Sets up `xcfName` for native targets.
        -   Configures source sets for `commonMain`, `commonTest`, `androidMain`, `androidDeviceTest`, and `iosMain`.
        -   Includes dependencies for `architecture` module in `commonMain`, `kotlin.test` in `commonTest`, and various testing dependencies in `androidDeviceTest`.
    -   Adds `androidDeviceTest` and `androidHostTest` source sets with example test.
    -   Adds `.gitignore` to the `settings` module.
    -   Updates `settings.gradle.kts` to include the new `settings` module.

---
+++ features/settings/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    alias(libs.plugins.playground.multiplatform.lib)
+    alias(libs.plugins.playground.compose.multiplatform)
+    alias(libs.plugins.playground.koin)
+    alias(libs.plugins.playground.navigation)
+    alias(libs.plugins.playground.networking)
+    alias(libs.plugins.playground.testing)
+    alias(libs.plugins.kotlinxSerialization)
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "com.zeyadgasser.playground.settings"
+
+    }
+    val xcfName = "settingsKit"
+    targets.filterIsInstance<KotlinNativeTarget>().forEach { target: KotlinNativeTarget ->
+        target.binaries.framework {
+            baseName = xcfName
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(project(":core:architecture"))
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+
+        androidMain {
+            dependencies {
+            }
+        }
+
+        getByName("androidDeviceTest") {
+            dependencies {
+                implementation(libs.androidx.runner)
+                implementation(libs.androidx.core)
+                implementation(libs.androidx.test.junit)
+            }
+        }
+
+        iosMain {
+            dependencies {
+            }
+        }
+    }
+}
