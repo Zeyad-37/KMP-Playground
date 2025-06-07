9c9b2863a73f9955f6e4d3613b910be3ac0a051b
zeyadg37 <zeyad.gasser@gmail.com>
2025-06-07 12:29:07 +0200

    feat: Refactor RoutineListViewModel to use InputHandler Map
    - Introduces `InputHandler` interface in the architecture module to define a standard way of processing inputs.
    - Modifies `RoutineListViewModel` to accept a map of `InputHandler`s.
    - Moves the logic for `LoadRoutineListInput` and `RoutineRatedInput` into `LoadRoutineListInputHandler` and `RateRoutineInputHandler` respectively.
    - Updates `routineListModule` to provide instances of these new handlers and inject the map of handlers into `RoutineListViewModel`.
    - The `resolve` method in `RoutineListViewModel` now delegates to the appropriate handler based on the input type, falling back to existing logic for unhandled inputs.

---
+++ features/routine/list/src/commonMain/kotlin/com/zeyadgasser/playground/routine/list/viewmodel/LoadRoutineListInputHandler.kt
@@ -0,0 +1,40 @@
+package com.zeyadgasser.playground.routine.list.viewmodel
+
+import com.zeyadgasser.playground.architecture.presentation.InputHandler
+import com.zeyadgasser.playground.architecture.presentation.Result
+import com.zeyadgasser.playground.routine.domain.RoutineRepository
+import com.zeyadgasser.playground.routine.list.viewmodel.RoutineListState.EmptyState
+import com.zeyadgasser.playground.routine.sharedpresentation.RoutinePM
+import com.zeyadgasser.playground.routine.sharedpresentation.RoutinePresentationMapper
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEmpty
+import kotlinx.coroutines.flow.onStart
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
+class LoadRoutineListInputHandler(
+    private val repository: RoutineRepository,
+    private val taskPresentationMapper: RoutinePresentationMapper,
+) : InputHandler<LoadRoutineListInput, RoutineListState> {
+
+    override suspend fun invoke(input: LoadRoutineListInput, currentState: RoutineListState): Flow<Result> =
+        flow {
+            val routine = taskPresentationMapper.toPresentationList(repository.getAllRoutines())
+            emit(
+                if (routine.isNotEmpty())
+                    LoadRoutineListResult(groupIntoCategories(routine), getCurrentDate())
+                else EmptyState
+            )
+        }.onEmpty { emit(LoadingResult(false)) }.onStart { emit(LoadingResult(true)) }
+
+    private fun groupIntoCategories(list: List<RoutinePM>): List<CategorisedRoutinePM> =
+        list.groupBy { it.category }.toMap()
+            .map { (category, articles) -> CategorisedRoutinePM(category, articles) }
+
+    private fun getCurrentDate(): String {
+        val time = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+        return "Today, ${time.date.dayOfMonth}, ${time.date.month.name}, ${time.date.year}"
+    }
+}
